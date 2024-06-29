a2afb1db313687851552ff3a800da679dee343dc
sebasdurango <sebasdurango19@gmail.com>
2024-06-29 18:34:32 -0500

    My First Component

---
+++ src/components/MyFirstComponent.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const MyFirstComponent = () => {
+  return (
+    <div>
+        <div>MyFirstComponent</div>
+        <div>Hello World, I'm Joan Sebastian Durango</div>
+        <div>27 de Junio de 2024</div>
+    </div>
+  )
+}
